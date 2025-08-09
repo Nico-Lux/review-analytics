3ca55cb0796195277533793a5492d82fe8ac96f6
Nico-Lux <soyluffy@gmail.com>
2025-08-09 23:01:12 +0800

    new version

--- reviews.py
+++ reviews.py
@@ -2,13 +2,21 @@ data = []
 count = 0
 with open('reviews.txt', 'r') as f:
 	for line in f:
-		data.append(line)
+		data.append(line.strip())
 		count = count+1
 		if count%1000 == 0:
 			print(len(data))
 
-print(len(data))
-print(data[0])
-print(data[1])
-print(data[0], data[1])
+print('檔案讀取完了，總共有：', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	n = len(d)
+	sum_len = sum_len + n
+
+print('平均長度為', sum_len/len(data))
+
+	
+ 
+
 
